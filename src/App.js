94d6d545bdd907ee67d547f69cc8716be86f622a
pravinshedbale <pravin.shedbale@outlook.com>
2023-08-28 22:47:01 +0530

    react quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
+const initialState = {
+    questions: [],
+    // ['loading', 'error', 'ready', 'active', 'finished']
+    status: "loading",
+    index: 0,
+    answer: null,
+    points: 0,
+    highScore: 0,
+};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "dataReceived":
+            return { ...state, questions: action.payload, status: "ready" };
+        case "dataFailed":
+            return { ...state, status: "error" };
+        case "start":
+            return { ...state, status: "active" };
+        case "newAnswer":
+            const question = state.questions.at(state.index);
+            return {
+                ...state,
+                answer: action.payload,
+                points:
+                    action.payload === question.correctOption
+                        ? state.points + question.points
+                        : state.points,
+            };
+        case "nextQuestion":
+            return { ...state, index: state.index + 1, answer: null };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        case "finish":
+            return {
+                ...state,
+                status: "finished",
+                highScore:
+                    state.points > state.highScore
+                        ? state.points
+                        : state.highScore,
+            };
+        default:
+            throw new Error("Action unknown");
+    }
+};
+const App = () => {
+    const [{ questions, status, index, answer, points, highScore }, dispatch] =
+        useReducer(reducer, initialState);
+    useEffect(() => {
+        fetch("http://localhost:9999/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({ type: "dataReceived", payload: data }))
+            .catch((err) => dispatch({ type: "dataFailed" }));
+    }, []);
+    const numQuestions = questions.length;
+    const maxPossiblePoints = questions.reduce(
+        (prev, cur) => prev + cur?.points,
+        0
+    );
+    return (
+        <div className="app">
+            <Header />
+            <Main>
+                {status === "loading" && <Loader />}
+                {status === "error" && <Error />}
+                {status === "ready" && (
+                    <StartScreen
+                        numQuestions={numQuestions}
+                        dispatch={dispatch}
+                    />
+                )}
+                {status === "active" && (
+                    <>
+                        <Progress
+                            numQuestions={numQuestions}
+                            index={index}
+                            points={points}
+                            maxPossiblePoints={maxPossiblePoints}
+                            answer={answer}
+                        />
+                        <Question
+                            question={questions[index]}
+                            dispatch={dispatch}
+                            answer={answer}
+                        />
+                        <NextButton
+                            dispatch={dispatch}
+                            answer={answer}
+                            index={index}
+                            numQuestions={numQuestions}
+                        />
+                    </>
+                )}
+                {status === "finished" && (
+                    <FinishScreen
+                        points={points}
+                        maxPossiblePoints={maxPossiblePoints}
+                        highScore={highScore}
+                    />
+                )}
+            </Main>
+        </div>
+    );
+};
 
 export default App;
